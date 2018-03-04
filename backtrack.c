a4f909e627465c90f77a469ddd1a40eefec8b477
Thome <thomeneto@hotmail.com>
2018-03-03 23:58:49 -0300

    Update backtrack.c

--- backtrack.c
+++ backtrack.c
@@ -4,45 +4,52 @@
 void processar(int x, int y, char** matriz, int altura, int largura, int cordx, int cordy, int* flag){
 
     if (x == altura-1 || x == 0 || y == largura-1 || y == 0){
-        printf("SAIDA [%d,%d]", x, y);
+        printf("SAIDA [%d,%d]\n", x, y);
         *flag = 1;
     } else {
         if (matriz[x][y+1] == '0'){
-            printf("D [%d,%d]->[%d,%d]", x, y, x, y+1);
-            matriz[x][y+1] == 'X';
+            printf("D [%d,%d]->[%d,%d]\n", x, y, x, y+1);
+            matriz[x][y+1] = 'X';
             processar(x, y+1, matriz, altura, largura, x, y, flag);
         }
 
         if (matriz[x-1][y] == '0'){
-            printf("F [%d,%d]->[%d,%d]", x, y, x-1, y);
-            matriz[x-1][y] == 'X';
+            printf("F [%d,%d]->[%d,%d]\n", x, y, x-1, y);
+            matriz[x-1][y] = 'X';
             processar(x-1, y, matriz, altura, largura, x, y, flag);
         }
 
         if (matriz[x][y-1] == '0'){
-            printf("E [%d,%d]->[%d,%d]", x, y, x, y-1);
-            matriz[x][y-1] == 'X';
+            printf("E [%d,%d]->[%d,%d]\n", x, y, x, y-1);
+            matriz[x][y-1] = 'X';
             processar(x, y-1, matriz, altura, largura, x, y, flag);
         }
 
         if (matriz[x+1][y] == '0'){
-            printf("T [%d,%d]->[%d,%d]", x, y, x+1, y);
-            matriz[x+1][y] == 'X';
+            printf("T [%d,%d]->[%d,%d]\n", x, y, x+1, y);
+            matriz[x+1][y] = 'X';
             processar(x+1, y, matriz, altura, largura, x, y, flag);
         }
 
         if (cordx != 0 && cordy != 0) { //não é a primeira instancia de processar
-            printf("BT [%d,%d]<-[%d,%d]", cordx, cordy, x, y);
+            printf("BT [%d,%d]<-[%d,%d]\n", cordx, cordy, x, y);
         } else if (*flag == 0) {         //é a primeira instancia de processar, E não achou saída
-            printf("SEM SAIDA");
+            printf("SEM SAIDA\n");
         }
     }
 
 
 }
 
+
+//int main(int argc, char* argv[]){
 int main(){
 
+	//printf("#ARGS = %i\n", argc);
+	//printf("PROGRAMA = %s\n", argv[0]);
+	//printf("ARG1 = %s, ARG2 = %s\n", argv[1], argv[2]);
+	//FILE* input = fopen(argv[1], "r");
+	//FILE* output = fopen(argv[2], "w");
 	FILE* input = fopen("input.txt", "r");
 
 	int NL;
@@ -65,18 +72,18 @@ int main(){
 				fscanf(input, "%c ", &matriz[i][j]);
 				//printf("%c ", matriz[i][j]);
 				if (matriz[i][j] == 'X'){
-				    a = i;
-				    b = j;
+                    a = i;
+                    b = j;
 				}
 
 			}
 			//printf("\n");
 		}
 
-		printf("L%d:\n", contador);
-		printf("INICIO [%d,%d]\n", a, b);
-		flag = 0;
-		processar(a, b, matriz, altura, largura, 0, 0, &flag);
+        printf("L%d:\n", contador);
+        printf("INICIO [%d,%d]\n", a, b);
+        flag = 0;
+        processar(a, b, matriz, altura, largura, 0, 0, &flag);
 
 
 		for(i = 0; i < altura; i++){
